c016976d2cf41d1498b2d7fbb8acc2e6fd89c356
silvaPool <silva.friends22@gmail.com>
2024-04-05 11:03:27 -0300

    Project trailer

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import axios, { Axios } from "axios";
+import movieTrailer from "movie-trailer";
+import { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
+
+function App() {
+  const [video, setVideo] = useState("inception");
+  const [videoURL, setVideoURL] = useState("https://youtu.be/sa9l-dTv9Gk");
+
+  function handleSearch() {
+    movieTrailer(video).then((res) => {
+      setVideoURL(res);
+    });
+  }
+
+  return (
+    <div className="App">
+      <div className="search-box">
+        <label>Search for any movies/shows: </label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setVideo(e.target.value);
+          }}
+        />
+        <button onClick={() => handleSearch()}>Search</button>
+      </div>
+
+      <ReactPlayer url={videoURL} controls={true} />
+    </div>
+  );
+}
+
+export default App;
